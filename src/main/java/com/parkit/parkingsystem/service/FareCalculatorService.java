df1105d9b05766c29039db4d94f979a6fc6d139c
HassanCHKIRROU <chkirrou.hassan@gmail.com>
2025-05-31 23:27:06 +0100

    Correction des durées de stationnement pour éviter des valeurs négatives

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -9,13 +9,17 @@ public class FareCalculatorService {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
+        /*
         int inHour = ticket.getInTime().getHours();
         int outHour = ticket.getOutTime().getHours();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
         int duration = outHour - inHour;
-
+        
+        **/
+        long durationInMillis = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+        double duration = (double)durationInMillis/(60*60*1000);
+  
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
